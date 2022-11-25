ecefdc37a26df43433af71e297eafaa1fa0d705a
LidiaMil <lidamilos@rambler.ru>
2022-11-26 01:42:59 +0300

    add about us

--- pasSec/main/views.py
+++ pasSec/main/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(req):
-    return HttpResponse("<h5>Hiiiiii</h5>");
+    return HttpResponse("<h5>Hiiiiii</h5>")
+
+def about(req):
+    return HttpResponse("<h6>About us</h6>")
