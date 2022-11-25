0780bfbd1a247afaa7f2f4b98cd08888b7650159
LidiaMil <lidamilos@rambler.ru>
2022-11-26 02:26:55 +0300

    user create

--- pasSec/main/models.py
+++ pasSec/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField('id', primary_key=True)
+    email = models.EmailField('email')
+    password = models.TextField('password')
+
+    def __str__(self):
+        return self.email
