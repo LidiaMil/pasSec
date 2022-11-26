b62d4d0452dc55ebe5921fd5c1d9d1393366a75c
LidiaMil <lidamilos@rambler.ru>
2022-11-26 03:29:39 +0300

    migration add

--- pasSec/main/models.py
+++ pasSec/main/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    id = models.UUIDField('id', primary_key=True)
+class Users(models.Model):
     email = models.EmailField('email')
     password = models.TextField('password')
 
