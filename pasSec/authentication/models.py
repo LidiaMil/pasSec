b536d7c38188a107b7402580ee438660376ad089
LidiaMil <lidamilos@rambler.ru>
2022-11-26 16:31:44 +0300

    add logic

--- pasSec/authentication/models.py
+++ pasSec/authentication/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 
-from ..core/models import TimestampedModel
+from pasSec.core.models import TimestampedModel
 
 
 class UserManager(BaseUserManager):
