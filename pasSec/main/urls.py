5780e70b118d5b2500b1acb7484545784f0db00e
LidiaMil <lidamilos@rambler.ru>
2022-11-26 03:29:48 +0300

    settings

--- pasSec/main/urls.py
+++ pasSec/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='settings'),
     path('about', views.about, name='about'),
+    path('create', views.create, name='create'),
 ]
--- pasSec/main/views.py
+++ pasSec/main/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Users
+from .forms import UsersForm
 # Create your views here.
 
 def index(req):
-    return render(req, "main/index.html")
+    users = Users.objects.all()
+    return render(req, "main/index.html", { 'title': 'Users All', 'Users': users})
 
 def about(req):
     return render(req,"main/about.html")
+
+def create(req):
+    error = ''
+    if req.method == 'POST':
+        form = UsersForm(req.POST)
+        print(req.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            error = 'invalid'
+
+
+    form = UsersForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(req,"main/create.html", context)
