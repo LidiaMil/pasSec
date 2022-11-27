65c06606e17d70478fe578a92dc3eda2bffb084d
LidiaMil <lidamilos@rambler.ru>
2022-11-27 13:38:28 +0300

    add pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Login, Profile, Register } from "./pages";
+import { CreatePassword, Login, PasswordList, Profile, Register } from "./pages";
 import store, { persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
@@ -16,6 +16,8 @@ export default function App() {
               <Route exact path="/login" component={Login} />
               <ProtectedRoute exact path="/" component={Profile} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/create" component={CreatePassword} />
+              <Route exact path="/list" component={PasswordList} />
             </Switch>
           </div>
         </Router>
