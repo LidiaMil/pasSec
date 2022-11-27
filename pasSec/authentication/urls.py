02d2ba0020aa34c41bd79afebd3c568f5bfb3fd6
LidiaMil <lidamilos@rambler.ru>
2022-11-27 03:04:34 +0300

    deleted

--- pasSec/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    # url('user/', views.UserRetrieveUpdateAPIView),
-    # path('register', views.RegistrationAPIView, name='register'),
-    # url('register/login/', views.LoginAPIView.as_view()),
-]
