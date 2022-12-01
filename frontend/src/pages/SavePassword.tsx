bfc6f680db68934e9a76c9662cf334cbbc2bc5ce
LidiaMil <lidamilos@rambler.ru>
2022-12-01 03:48:02 +0300

    start pages

--- frontend/src/pages/SavePassword.tsx
+++ frontend/src/pages/SavePassword.tsx
@@ -12,7 +12,7 @@ interface LocationState {
 }
 
 
-const Profile = () => {
+const SavePassword = () => {
   const account = useSelector((state: RootState) => state.auth.account);
   const dispatch = useDispatch();
   const history = useHistory();
@@ -25,6 +25,24 @@ const Profile = () => {
     dispatch(authSlice.actions.setLogout());
     history.push("/login");
   };
+
+  const formik = useFormik({
+    initialValues: {
+      username: "l",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      setLoading(true);
+      handleSingUp(values.username, values.email, values.password);
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().trim().required("required field"),
+      email: Yup.string().trim().required("required field"),
+      password: Yup.string().trim().required("required field"),
+    }),
+  });
+
   return (
     <div className="w-full h-screen">
       <div className="w-full p-6">
@@ -48,17 +66,67 @@ const Profile = () => {
         </button>
       </div>
     </header>
+    <div className="h-screen flex bg-gray-bg1">
+      <div className="w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16">
+        <h1 className="text-2xl font-medium text-primary mt-4 mb-12 text-center">
+           register in our app 🔐
+        </h1>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="space-y-4">
+          <input
+              className="border-b border-gray-300 w-full px-2 h-8 rounded focus:border-blue-500"
+              id="username"
+              type="username"
+              placeholder="Username"
+              name="username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.errors.email ? <div>{formik.errors.email} </div> : null}
+            <input
+              className="border-b border-gray-300 w-full px-2 h-8 rounded focus:border-blue-500"
+              id="email"
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.errors.email ? <div>{formik.errors.email} </div> : null}
+            <input
+              className="border-b border-gray-300 w-full px-2 h-8 rounded focus:border-blue-500"
+              id="password"
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.errors.password ? (
+              <div>{formik.errors.password} </div>
+            ) : null}
+          </div>
+          <div className="text-danger text-center my-2" hidden={false}>
+            {message}
+          </div>
+
+          <div className="flex justify-center items-center mt-6">
+            <button
+              type="submit"
+              disabled={loading}
+              className="rounded border-gray-300 p-2 w-32 bg-blue-700 text-white"
+            >
+              Регистрация
+            </button>
+            <a href="/login">Я уже зарегестрирован</a>
+          </div>
+        </form>
       </div>
-        {
-            user.data ?
-                <div className="w-full h-full text-center items-center">
-                    <p className="self-center my-auto">Welcome, {user.data?.username}</p>
-                </div>
-                :
-                <p className="text-center items-center">Loading ...</p>
-        }
     </div>
   );
 };
 
-export default Profile;
+export default SavePassword;
